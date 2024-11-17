8c59c9327627ea12a0612d9aea163ac120fcd2ec
Mihai-Simedrea <mihai.nicolae.simedrea@gmail.com>
2024-11-17 14:56:21 +0200

    feat: member init list

--- main.cpp
+++ main.cpp
@@ -1,15 +1,57 @@
 #include <iostream>
+#include <string>
+#include <list>
+
+// but for consistency, it's often best to initialize everything via member initialization.
+
+class PhoneNumber {
+    public:
+        PhoneNumber(const std::string& number) : number_(number) {}
+        std::string getNumber() const { return number_; }
+    private:
+        std::string number_;
+};
+
+class ABEntry {
+    public:
+        ABEntry(const std::string& name, const std::string& address, const std::list<PhoneNumber>& phones)
+            : theName(name),
+            theAddress(address),
+            thePhones(phones),
+            numTimesConsulted(0)
+            // member init
+        {}
+
+        void display() const {
+            std::cout << "Name: " << theName << "\nAddress: " << theAddress << "\nPhones: ";
+            for (const auto& phone : thePhones) {
+                std::cout << phone.getNumber() << " ";
+            }
+            std::cout << "\nConsulted: " << numTimesConsulted << " times\n";
+        }
+
+    private:
+        std::string theName;
+        std::string theAddress;
+        std::list<PhoneNumber> thePhones;
+        int numTimesConsulted;
+};
+
+
+
+/*
+Fortunately, a small design change eliminates the problem entirely.
+All that has to be done is to move each non-local static object into its
+
+own function, where it’s declared static.
+*/
 
 int main(int argc, char** argv) {
     // Item 4
 
-    int x = 0; // manual initialization of an int
-    const char * text = "A C-style string"; // manual initialization of a
-    // pointer (see also Item 3)
-    double d; // “initialization” by reading from
-    std::cin >> d;
-
-    std::cout << d << std::endl;
+    std::list<PhoneNumber> phones = { PhoneNumber("+40724231204"), PhoneNumber("+40724531241") };
+    ABEntry entry("Steve Jobs", "2101 Waverley St, Palo Alto, CA", phones);
+    entry.display();
 
     return 0;
 }
